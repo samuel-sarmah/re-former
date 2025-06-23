72b0491589a158c95c09611eff621ff8454af9b6
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-23 16:20:38 +0300

    Create a private user_params method to expect and filter the incoming attributes hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+  # Gives us back the hash containing the params we need to create or update a post
+  def user_params
+    params.expect(user: [ :email, :username, :password ])
   end
 end
