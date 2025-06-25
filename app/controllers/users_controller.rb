a6572e0bfd9040059dfb74d3031c063bab672968
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-25 21:52:09 +0300

    Remove update action associated with edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   private
   # Gives us back the hash containing the params we need to create or update a post
   def user_params
