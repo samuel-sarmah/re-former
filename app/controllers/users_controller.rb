516699869b19bc590b36961591d9a5a17120a1d7
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-25 21:21:56 +0300

    feat: add update action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   # Gives us back the hash containing the params we need to create or update a post
   def user_params
