a2d8d0f1a1341602a8f48f1336641f26220f8fda
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-06-25 17:48:53 +0300

    Feat: add edit action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
   # Gives us back the hash containing the params we need to create or update a post
   def user_params
